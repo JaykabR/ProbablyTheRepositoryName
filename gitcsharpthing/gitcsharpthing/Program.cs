7e6f63901a362fbf6cc1bb709096fbcace7b8fe5
Jay <102559627@student.swin.edu.au>
2019-08-21 17:28:29 +1000

    Git ignore and c# project

---
+++ gitcsharpthing/gitcsharpthing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace gitcsharpthing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("i wanna dieeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee!");
+        }
+    }
+}
